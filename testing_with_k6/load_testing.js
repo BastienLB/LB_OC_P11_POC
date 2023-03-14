85a584a1f735ad44a905ca06aa4d3f00de45ac0f
BastienLBCH <bastien.labouche@live.fr>
2023-03-14 16:02:47 +0100

    Create load_testing.js

---
+++ testing_with_k6/load_testing.js
@@ -0,0 +1,22 @@
+import http from 'k6/http';
+
+export const options = {
+    thresholds: {
+        http_req_failed: ['rate<0.01'], // http errors should be less than 1%
+        http_req_duration: ['p(95)<200'], // 95% of requests should be below 200ms
+    },
+    scenarios: {
+        my_scenario1: {
+            executor: 'constant-arrival-rate',
+            duration: '3m',                    // total duration
+            preAllocatedVUs: 50,                // to allocate runtime resources
+
+            rate: 50,                           // number of constant iterations given `timeUnit`
+            timeUnit: '1s',
+        },
+    },
+};
+
+export default function () {
+    http.get('http://node28791-env-6815870.rag-cloud.hosteur.com/dev-poc-siu/hospitals/51.437195/-2.847193');
+}
