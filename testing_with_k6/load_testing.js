846cf119ef2b466cddb0868980b304ffa422239d
BastienLBCH <32386403+BastienLB@users.noreply.github.com>
2023-03-15 11:50:57 +0100

    updated tests and app deployment

--- testing_with_k6/load_testing.js
+++ testing_with_k6/load_testing.js
@@ -1,27 +1,41 @@
 import http from 'k6/http';
 import { check } from 'k6';
+import { sleep } from 'k6';
 
 export const options = {
     thresholds: {
         http_req_failed: ['rate<0.01'], // http errors should be less than 1%
         http_req_duration: ['p(95)<200'], // 95% of requests should be below 200ms
     },
+    discardResponseBodies: true,
     scenarios: {
-        my_scenario1: {
-            executor: 'constant-arrival-rate',
-            duration: '5m',                    // total duration
-            preAllocatedVUs: 50,                // to allocate runtime resources
-            maxVUs: 8000,
-
-            rate: 8000,                           // number of constant iterations given `timeUnit`
-            timeUnit: '1s',
+        contacts: {
+            executor: 'ramping-vus',
+            startVUs: 0,
+            stages: [
+                { duration: '1m', target: 1000 },
+                { duration: '1m', target: 2000 },
+                { duration: '1m', target: 3000 },
+                { duration: '1m', target: 4000 },
+                { duration: '1m', target: 5000 },
+                { duration: '1m', target: 6000 },
+                { duration: '1m', target: 7000 },
+                { duration: '1m', target: 8000 },
+                { duration: '30s', target: 8000 },
+                { duration: '10s', target: 0 },
+            ],
+            gracefulRampDown: '0s',
         },
     },
 };
 
+
 export default function () {
     const res = http.get('http://node28791-env-6815870.rag-cloud.hosteur.com/dev-poc-siu/hospitals/51.437195/-2.847193');
     check(res, {
-        'is status 200': (r) => r.status === 200,
+      'is status 200': (r) => r.status === 200,
     });
+    // Virtual User make 4 request per second
+    sleep(0.5);
 }
+
