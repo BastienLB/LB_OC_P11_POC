0a8fe58731b00398469b91f1207ca7303bd3fc62
BastienLBCH <32386403+BastienLB@users.noreply.github.com>
2023-03-20 17:27:00 +0100

    Update test.sh

--- testing_app/testing_reliability/test.sh
+++ testing_app/testing_reliability/test.sh
@@ -21,6 +21,7 @@ count_online_containers(){
     done
     echo "$counter"
 }
+offline_containers_initial_count="$(count_offline_containers)"
 
 echo "Sending a request to shut down at least one container"
 curl http://node28791-env-6815870.rag-cloud.hosteur.com/dev-poc-siu/stop-spring
@@ -30,26 +31,31 @@ echo "Check every second if a container has been shut down"
 restarted=false
 for i in {1..50}
 do
-  count="$(count_offline_containers)"
-  if [ "$count" -ge 1 ]
+  # Check if there's more offline containers after sending the test request
+  # meaning at least one container has been shut down
+  actually_offline_containers="$(count_offline_containers)"
+  if [ "$actually_offline_containers" -ge "$offline_containers_initial_count" ]
   then
       restarted=true
+      online_containers_after_restart="$(count_online_containers)"
   fi
   sleep 1
 done
 
+# If no container has been shut down the test has fail
 if [ $restarted == false ]
 then
   echo "No container has restarted"
   exit 1
 fi
+
 echo "At least one container has shut down"
 echo "Waiting 4 minutes to let time for pods to restart"
 sleep 240
 
-echo "Verifying if all pods are up and running"
-count="$(count_online_containers)"
-if [ "$count" -lt 5 ]
+echo "Verifying container has restarted"
+online_containers="$(count_online_containers)"
+if [ "$online_containers" -lt "$online_containers_after_restart" ]
 then
     exit 1
 fi
